faede3915b5ff58167b7755ee7efc88214baae4f
Ksusie0214 <tdlrtmvldj@gmail.com>
2024-03-20 16:21:35 +0900

    vue / 데이터 바인딩

---
+++ src/views/Ex01View.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>vue공부</h1>
+    <h2>{{ subTitle }}</h2>
+    <p>{{ content }}</p>
+</template>
+
+<script>
+export default {
+    name: "Ex01View",
+    components: {},
+    data() {
+        return {
+            subTitle: "바인딩된 소제목",
+            content : "죽을때까지 끄투하자"
+        };
+    },
+    methods: {},
+};
+</script>
+<style></style>
